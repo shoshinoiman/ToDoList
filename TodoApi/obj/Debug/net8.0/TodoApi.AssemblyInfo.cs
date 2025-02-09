13803aeb4d668ec94ff00257b620579db30f8c08
soshinoiman <your-email@example.com>
2025-02-09 18:42:15 +0200

    Remove ToDoListReact-master from tracking

--- TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da4039a6483757495480b7e4c4046668841893e2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ded01197d3ad93b73f4d10aa68f252375481275c")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

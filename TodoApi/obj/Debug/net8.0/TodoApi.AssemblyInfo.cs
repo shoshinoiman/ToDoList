e8830b4fc932ba121e3202ca20ed411d0e0ab614
soshinoiman <your-email@example.com>
2025-02-10 01:38:17 +0200

    docker2

--- TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3afad5ae5896f15c70cf5c9b4fd342eb05136bce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66acbe167f534d8428ca1f8cd885c38d78b11d0a")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
